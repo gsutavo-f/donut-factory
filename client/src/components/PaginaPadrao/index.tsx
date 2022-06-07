4f1010e8350b4fa17e2633e7bfe13c32713ebca4
jun1rox <junior_alaniz@hotmail.com>
2022-06-07 20:42:02 -0300

    Modal funcionário

--- client/src/components/PaginaPadrao/index.tsx
+++ client/src/components/PaginaPadrao/index.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 export default function PaginaPadrao() {
   return (
     <>
-      <div>
+      <div className={styles.container}>
         <Outlet />
       </div>
     </>
---
+++ client/src/pages/Funcionarios/FuncionariosForm/index.tsx
@@ -0,0 +1,97 @@
+import Axios from 'axios';
+
+interface IFuncionariosForm {
+  name: string;
+  setName: React.Dispatch<React.SetStateAction<string>>;
+  cpf: string;
+  setCpf: React.Dispatch<React.SetStateAction<string>>;
+  position: string;
+  setPosition: React.Dispatch<React.SetStateAction<string>>;
+  salary: number;
+  setSalary: React.Dispatch<React.SetStateAction<number>>;
+  filial: number;
+  setFilial: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function FuncionariosForm(
+  { name, setName,
+    cpf, setCpf,
+    position, setPosition,
+    salary, setSalary,
+    filial, setFilial }: IFuncionariosForm) {
+
+  function adicionarFuncionario(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+
+    Axios.post('http://localhost:3001/funcionario/create', {
+      name: name,
+      cpf: cpf,
+      position: position,
+      salary: salary,
+      filial: filial
+    }).then(() => {
+      console.log("sucess");
+    });
+  }
+
+  return (
+    <form onSubmit={adicionarFuncionario}>
+      <label htmlFor="nome">
+        Nome
+      </label>
+      <input
+        type="text"
+        name="nome"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        placeholder="Nome completo"
+        required
+      />
+      <label htmlFor="cpf">
+        Cpf
+      </label>
+      <input
+        type="text"
+        name="cpf"
+        value={cpf}
+        onChange={(event) => setCpf(event.target.value)}
+        placeholder="Apenas números"
+        required
+      />
+      <label htmlFor="cargo">
+        Cargo
+      </label>
+      <input
+        type="text"
+        name="cargo"
+        value={position}
+        onChange={(event) => setPosition(event.target.value)}
+        required
+      />
+      <label htmlFor="salario">
+        Salário
+      </label>
+      <input
+        type="number"
+        name="salario"
+        value={salary}
+        onChange={(event) => setSalary(event.target.valueAsNumber)}
+        required
+      />
+      <label htmlFor="filial">
+        Filial
+      </label>
+      <input
+        type="number"
+        name="filial"
+        value={filial}
+        onChange={(event) => setFilial(event.target.valueAsNumber)}
+        placeholder="Código da filial"
+        required
+      />
+      <button type="submit">
+        Adicionar funcionário
+      </button>
+    </form>
+  );
+}
